2c49ed09f6f8caec163dd484215751af1f6cdc2f
Vishrut2403 <vishrutsachan2004@gmail.com>
2025-07-13 14:13:38 +0530

    Initial Commit

---
+++ src/Experience/sources.js
@@ -0,0 +1,288 @@
+export default [
+    // Models
+    {
+        name: 'ramenShopModel',
+        type: 'gltfModel',
+        path: 'models/ramenShop/glTF/ramenShop.gltf'
+    },
+    {
+        name: 'ramenHologram',
+        type: 'gltfModel',
+        path: 'models/ramenShop/glTF/ramenHologram.gltf'
+    },
+    // Baked Textures
+    {
+        name: 'ramenShopBakedTexture',
+        type: 'KTX2Texture',
+        path: 'textures/baked/ramenShopBaked1024.ktx2'
+    },
+    {
+        name: 'machinesBakedTexture',
+        type: 'KTX2Texture',
+        path: 'textures/baked/machinesBaked1024.ktx2'
+    },
+
+    {
+        name: 'floorBakedTexture',
+        type: 'KTX2Texture',
+        path: 'textures/baked/floorBaked1024.ktx2'
+    },
+
+    {
+        name: 'miscBakedTexture',
+        type: 'KTX2Texture',
+        path: 'textures/baked/miscBaked1024.ktx2'
+    },
+
+    {
+        name: 'graphicsBakedTexture',
+        type: 'KTX2Texture',
+        path: 'textures/baked/graphicsBaked512.ktx2'
+    },
+    // Matcaps
+    {
+        name: 'dishMatcapTexture',
+        type: 'KTX2Texture',
+        path: 'textures/matcaps/dishMatCap.ktx2'
+    },
+    {
+        name: 'fanMatcapTexture',
+        type: 'KTX2Texture',
+        path: 'textures/matcaps/fanMatCap.ktx2'
+    },
+    {
+        name: 'lightMatcapTexture',
+        type: 'texture',
+        path: 'textures/matcaps/lightMatcap.png'
+    },
+    {
+        name: 'neonBlueMatcapTexture',
+        type: 'KTX2Texture',
+        path: 'textures/matcaps/neonBlueMatCap.ktx2'
+    },
+    {
+        name: 'neonGreenMatcapTexture',
+        type: 'KTX2Texture',
+        path: 'textures/matcaps/neonGreenMatCap.ktx2'
+    },
+    // Arcade Screens
+    {
+        name: 'arcadeScreenDefaultTexture',
+        type: 'texture',
+        path: 'textures/screens/arcadeScreens/arcadeScreenDefault.png'
+    },
+
+    {
+        name: 'arcadeScreenCreditsTexture',
+        type: 'texture',
+        path: 'textures/screens/arcadeScreens/arcadeScreenCredits.png'
+    },
+
+    {
+        name: 'arcadeScreenThanksTexture',
+        type: 'texture',
+        path: 'textures/screens/arcadeScreens/arcadeScreenThanks.png'
+    },
+
+    // Easel
+    {
+        name: 'easelTouchTexture',
+        type: 'KTX2Texture',
+        path: 'textures/screens/easel/easelTouch.ktx2'
+    },
+
+    {
+        name: 'easelClickTexture',
+        type: 'KTX2Texture',
+        path: 'textures/screens/easel/easelClick.ktx2'
+    },
+    
+
+    // About Me Screens
+    {
+        name: 'bigScreenDefaultTexture',
+        type: 'KTX2Texture',
+        path: 'textures/screens/aboutMeScreens/bigScreenDefault.ktx2'
+    },
+
+    {
+        name: 'bigScreenAboutMeTexture',
+        type: 'texture',
+        path: 'textures/screens/aboutMeScreens/bigScreenAbout.png'
+    },
+
+    {
+        name: 'bigScreenSkillsTexture',
+        type: 'texture',
+        path: 'textures/screens/aboutMeScreens/bigScreenSkills.png'
+    },
+
+    {
+        name: 'bigScreenExperienceTexture',
+        type: 'texture',
+        path: 'textures/screens/aboutMeScreens/bigScreenExperience.png'
+    },
+
+    {
+        name: 'bigScreenAboutMeMobileTexture',
+        type: 'texture',
+        path: 'textures/screens/aboutMeScreens/bigScreenAboutMobile.png'
+    },
+
+    {
+        name: 'bigScreenSkillsMobileTexture',
+        type: 'texture',
+        path: 'textures/screens/aboutMeScreens/bigScreenSkillsMobile.png'
+    },
+
+    {
+        name: 'bigScreenExperienceMobileTexture',
+        type: 'texture',
+        path: 'textures/screens/aboutMeScreens/bigScreenExperienceMobile.png'
+    },
+
+    // Projects
+    {
+        name: 'vendingMachineDefaultTexture',
+        type: 'KTX2Texture',
+        path: 'textures/screens/vendingMachineScreens/vendingMachineDefault.ktx2'
+    },
+
+    {
+        name: 'vendingMachineMenuTexture',
+        type: 'texture',
+        path: 'textures/screens/vendingMachineScreens/vendingMachineMenu.png'
+    },
+
+
+    {
+        name: 'project1Texture',
+        type: 'KTX2Texture',
+        path: 'textures/screens/vendingMachineScreens/project1.ktx2'
+    },
+
+    {
+        name: 'project2Texture',
+        type: 'texture',
+        path: 'textures/screens/vendingMachineScreens/project2.png'
+    },
+
+    {
+        name: 'project3Texture',
+        type: 'texture',
+        path: 'textures/screens/vendingMachineScreens/project3.png'
+    },
+
+    {
+        name: 'project4Texture',
+        type: 'texture',
+        path: 'textures/screens/vendingMachineScreens/project4.png'
+    },
+
+
+    // Videos
+    {
+        name: 'littleTVScreenVideoTexture',
+        type: 'texture',
+        path: 'textures/videosTextures/littleTVScreen.png'
+    },
+    {
+        name: 'tallScreenVideoTexture',
+        type: 'videoTexture',
+        path: 'textures/videosTextures/pen.mp4'
+    },
+    {
+        name: 'tvScreenVideoTexture',
+        type: 'videoTexture',
+        path: 'textures/videosTextures/tvScreen.mp4'
+    },
+    {
+        name: 'smallScreen5VideoTexture',
+        type: 'texture',
+        path: 'textures/videosTextures/smallScreen5.png'
+    },
+    {
+        name: 'smallScreen4VideoTexture',
+        type: 'texture',
+        path: 'textures/videosTextures/smallScreen4.png'
+    },
+    {
+        name: 'smallScreen3VideoTexture',
+        type: 'texture',
+        path: 'textures/videosTextures/smallScreen3.png'
+    },
+
+    // smallScreen1 carousel
+
+    {
+        name: 'smallScreenOne1',
+        type: 'texture',
+        path: 'textures/screens/smallScreen1/1_Birdhouse_4K.png'
+    },
+    {
+        name: 'smallScreenOne2',
+        type: 'texture',
+        path: 'textures/screens/smallScreen1/2_Rockyriver_4K.png'
+    },
+    {
+        name: 'smallScreenOne3',
+        type: 'texture',
+        path: 'textures/screens/smallScreen1/3_Moon Rise_4K.png'
+    },
+    {
+        name: 'smallScreenOne4',
+        type: 'texture',
+        path: 'textures/screens/smallScreen1/4_Moon Rise_4K_1.png'
+    },
+    {
+        name: 'smallScreenOne5',
+        type: 'texture',
+        path: 'textures/screens/smallScreen1/5_Sci-Fi Environment.png'
+    },
+
+    // smallScreen2 carousel
+
+    {
+        name: 'smallScreenTwo1',
+        type: 'texture',
+        path: 'textures/screens/smallScreen2/1_Birdhouse_4K.png'
+    },
+    {
+        name: 'smallScreenTwo2',
+        type: 'texture',
+        path: 'textures/screens/smallScreen2/2_Rockyriver_4K.png'
+    },
+    {
+        name: 'smallScreenTwo3',
+        type: 'texture',
+        path: 'textures/screens/smallScreen2/3_Moon Rise_4K.png'
+    },
+    {
+        name: 'smallScreenTwo4',
+        type: 'texture',
+        path: 'textures/screens/smallScreen2/4_Moon Rise_4K_1.png'
+    },
+    {
+        name: 'smallScreenTwo5',
+        type: 'texture',
+        path: 'textures/screens/smallScreen2/5_Sci-Fi Environment.png'
+    },
+
+    // sideScreen
+
+    {
+        name: 'sideScreen1Texture',
+        type: 'KTX2Texture',
+        path: 'textures/screens/sideScreens/sideScreen1.ktx2'
+    },
+    {
+        name: 'sideScreen2Texture',
+        type: 'KTX2Texture',
+        path: 'textures/screens/sideScreens/sideScreen2.ktx2'
+    },
+    {
+        name: 'sideScreen3Texture',
+        type: 'KTX2Texture',
+        path: 'textures/screens/sideScreens/sideScreen3.ktx2'
+    }
+]
